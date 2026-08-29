418a5339ecb85f3bd751983884acaeac4ec092cc
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2026-08-29 11:18:40 +0300

    build: Add native Linux Payjoin CI job

---
+++ ci/test/00_setup_env_native_payjoin.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_native_payjoin
+export CI_IMAGE_NAME_TAG="mirror.gcr.io/rust:1.85.1-bookworm"
+export APT_LLVM_V="17"
+export PACKAGES="clang-${APT_LLVM_V} llvm-${APT_LLVM_V} libboost-dev libsqlite3-dev"
+export NO_DEPENDS=1
+export GOAL="all"
+export CARGO_BUILD_JOBS="$(nproc)"
+export RUN_FUNCTIONAL_TESTS=false
+export BITCOIN_CONFIG="\
+ -DCMAKE_BUILD_TYPE=Debug \
+ -DCMAKE_C_COMPILER=clang \
+ -DCMAKE_CXX_COMPILER=clang++ \
+ -DENABLE_IPC=OFF \
+ -DENABLE_PAYJOIN=ON \
+"
